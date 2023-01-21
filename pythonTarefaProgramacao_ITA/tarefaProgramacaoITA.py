23d605cccea89b934b4ff2ea5363610a8c82df31
Andrehlb (Andre Luiz Barbosa) <andrehlbarbosa@gmail.com>
2023-01-21 17:31:14 -0300

    adding tarefaProgramacaoITA.py

---
+++ pythonTarefaProgramacao_ITA/tarefaProgramacaoITA.py
@@ -0,0 +1,7 @@
+valorLadoQuadrado = float(input("Digite o valor correspondente ao lado de um quadrado: "))
+
+areaQuadrado = valorLadoQuadrado ** 2
+
+perimetroQuadrado = valorLadoQuadrado * 4
+
+print("perímetro: ", perimetroQuadrado, " - área: ", areaQuadrado)
