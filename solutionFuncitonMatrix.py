9c5eff91d6b262339169f94191cfe40b0c8c4a97
Andrehlb <andrehlb@me.com>
2023-08-19 14:48:14 -0300

    Update the comment and iserting the one who proposed the challenge - solutionFunctionMatrix.py

--- solutionFuncitonMatrix.py
+++ solutionFuncitonMatrix.py
@@ -1,3 +1,4 @@
+# Desafio proposto por: Sr View | djairdj LinkedIn: https://www.linkedin.com/in/djairdj/
 # Crie uma função chamada "solution", com um parâmetro do tipo matriz.
 # Sua função irá receber uma matriz de números inteiros k, onde -10³ ≤ k ≤ 10³.
 # Essa matriz terá duas dimensões e será de ordem qualquer.
@@ -12,7 +13,8 @@
 # }
 # output:
 # {2, 4, 5, 7, 8}
-
+# Código em Python 3.8.2 criado por: André Luiz Barbosa | Andrehlb
+# Data: 19/08/2023
 def solution(matrix):
     # min(iterable, *[, default=obj, key=func]) -> value min(arg1, arg2, *args, *[, key=func]) -> value
     min_num = min(min(row) for row in matrix)
