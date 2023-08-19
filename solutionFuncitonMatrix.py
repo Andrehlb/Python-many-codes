c44518e683c9e8b37ec0ee2649dfacc2d7c57d1a
Andrehlb <andrehlb@me.com>
2023-08-19 14:37:04 -0300

    Adding the Python code using a function and two-dimensional matrix, termed solutionFunctionMatrix.py

---
+++ solutionFuncitonMatrix.py
@@ -0,0 +1,31 @@
+# Crie uma função chamada "solution", com um parâmetro do tipo matriz.
+# Sua função irá receber uma matriz de números inteiros k, onde -10³ ≤ k ≤ 10³.
+# Essa matriz terá duas dimensões e será de ordem qualquer.
+# Sua função terá que retorna um array (em ordem crescente) que contenha os números
+# que faltam para completar a sequência que vem na matriz, desde o menor até o maior número nela.
+# Caso não haja nenhum número faltando no intervalo, retornar um array vazio.
+# Exemplo:
+# input:
+# {
+#   {1, 3},
+#   {6, 9}
+# }
+# output:
+# {2, 4, 5, 7, 8}
+
+def solution(matrix):
+    # min(iterable, *[, default=obj, key=func]) -> value min(arg1, arg2, *args, *[, key=func]) -> value
+    min_num = min(min(row) for row in matrix)
+    max_num = max(max(row) for row in matrix)
+
+    numbers_in_matrix = [num for row in matrix for num in row]
+
+    missing_numbers = [num for num in range(min_num, max_num + 1) if num not in numbers_in_matrix]
+
+    return missing_numbers
+
+matrix = [
+    [1, 3],
+    [6, 9]
+]
+print(solution(matrix))
