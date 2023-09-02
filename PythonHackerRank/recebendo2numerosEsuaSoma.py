5bfdba5217c70886580a1b6d76daf0fe3b2e9ce0
Andrehlb <andrehlb@me.com>
2023-09-02 10:14:26 -0300

    adding the intgerConditional-oddWeird.py

---
+++ PythonHackerRank/recebendo2numerosEsuaSoma.py
@@ -0,0 +1,11 @@
+# Entradas
+# Pedir dois números para o usuário
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+# Processamento
+# Somar os dois números
+
+# Saída
+# Mostrar Resultado na Tela
+
