9ccc4533fc6d5cb991e99864817dc074e9f48a7c
Andrehlb <andrehlb@me.com>
2023-09-08 05:39:53 -0300

    Adding the 'recebendo2numerosEsuaSoma.py'' - PythonRackerHanck

--- PythonHackerRank/recebendo2numerosEsuaSoma.py
+++ PythonHackerRank/recebendo2numerosEsuaSoma.py
@@ -5,7 +5,9 @@ numero2 = int(input("Digite o segundo número: "))
 
 # Processamento
 # Somar os dois números
+resultado = numero1 + numero2
 
 # Saída
 # Mostrar Resultado na Tela
+print(f"A soma de {numero1} + {numero2} é {resultado}")
 
