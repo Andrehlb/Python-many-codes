92d104c5363a6031824d9c2f04ee214826cbbf8e
Andrehlb <andrehlb@me.com>
2024-03-26 11:33:20 -0300

    feat(create): Add `soma DeDoisNumeros.py` for newbies.

---
+++ somaDeDoisNumeros.py
@@ -0,0 +1,8 @@
+# Soma de dois números para iniciantes em Python
+# Programa que lê dois números inteiros e imprime a soma deles.
+
+# Declare A, B, X
+A = int(input())  # Leia A
+B = int(input())  # Leia B
+X = A + B         # Soma de A e B
+print("X = %d" % X)
